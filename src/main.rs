d8f3f5d15f60299283042b80fe38e3a3e8830063
isabel <isabel@isabelroses.com>
2024-06-13 10:25:07 +0100

    feat: init cli framework

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use clap::{CommandFactory, Parser};
+use clap_complete::generate;
 use log::trace;
+
+mod cli;
+use cli::{Cli, Commands};
 
 fn main() {
     saku_logger::init();
     trace!("logger init");
-    println!("Hello, world!");
+
+    let args = Cli::parse();
+    //println!("{:?}", args);
+
+    match args.command {
+        // Generate the completions and exit immediately
+        Some(Commands::Completions { shell }) => {
+            let mut cmd = Cli::command();
+            let name = cmd.get_name().to_string();
+            eprintln!("Generating completions for {shell}");
+            generate(shell, &mut cmd, name, &mut std::io::stdout());
+            std::process::exit(0);
+        }
+        _ => {
+            println!("Hello, world!");
+        }
+    }
 }
