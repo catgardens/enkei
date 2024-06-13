26de468052cf2f8fc83478de6ec59dedc7736d13
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-13 20:43:27 +0200

    feat: add `project()` util

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,4 +1,9 @@
 use crate::board::Board;
+use directories::ProjectDirs;
+
+pub fn project() -> anyhow::Result<ProjectDirs> {
+    ProjectDirs::from("dev", "catgardens", "enkei").ok_or(anyhow::format_err!("can not construct project directories"))
+}
 
 pub mod cli;
 mod impl_self;
