ef9f278800750f6c555f3903f48b30007c07becc
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-19 11:51:13 +0200

    feat: add usefull derives

--- src/board.rs
+++ src/board.rs
@@ -1,15 +1,11 @@
 use crate::item::Item;
 
+#[derive(Debug, Default)]
 pub struct Board {
     pub items: Vec<Item>,
 }
 
 impl Board {
-    pub fn default() -> Self {
-        Board {
-            items: Vec::new(),
-        }
-    }
     pub fn load_state() -> Self {
         todo!()
     }
