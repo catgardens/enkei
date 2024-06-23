8b63cb3c3777fadc0ebf03cbf047b9fa7a923838
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-23 15:10:25 +0200

    feat: implement simple item list

--- src/app/impl_view.rs
+++ src/app/impl_view.rs
@@ -1,9 +1,34 @@
+use cursive::{Vec2, XY};
 use cursive::view::View;
+
+use crate::{board::Board, item::Item};
 
 use super::App;
 
+impl View for Item {
+    fn draw(&self, printer: &cursive::Printer) {
+        printer.print(XY::zero(), &self.name);
+    }
+    fn required_size(&mut self, _constraint: Vec2) -> Vec2 {
+        Vec2::new(self.name.len(), 1)
+    }
+}
+
+impl View for Board {
+    fn draw(&self, printer: &cursive::Printer) {
+        let mut offset = Vec2::zero();
+        self.items.iter().for_each(|item| {
+            item.draw(&printer.offset(offset));
+            offset = offset.map_y(|v| v + item.clone().required_size(printer.size).y);
+        });
+    }
+}
+
 impl View for App {
     fn draw(&self, printer: &cursive::Printer) {
-        todo!()
+        self.board.draw(&printer);
+    }
+    fn required_size(&mut self, _constraint: cursive::Vec2) -> Vec2 {
+        Vec2::new(90, 20)
     }
 }
