c1b907f983a4c15f01478bdc517bd2aa7427c84f
isabel <isabel@isabelroses.com>
2024-07-29 18:51:16 +0100

    docs: macos files

--- src/app/impl_self.rs
+++ src/app/impl_self.rs
@@ -2,24 +2,29 @@ use crate::app::App;
 use crate::board::Board;
 use cursive::views::LinearLayout;
 
-impl App {
-    pub fn new() -> Self {
+impl Default for App {
+    fn default() -> Self {
         App {
             board: Board::default(),
             focus: 0,
             inner_view: Box::new(LinearLayout::vertical()),
         }
     }
+}
+
+impl App {
     /// initializes application state
     ///
-    /// - loads state from `~/.local/share/enkei/state.json`
+    /// - loads state from `~/.local/share/enkei/state.json` on linux
+    /// - or `~/Library/Application Support/dev.catgardens.eneki/state.json` on macos
     pub fn init(&mut self) -> anyhow::Result<()> {
         self.board = Board::load_state()?;
         Ok(())
     }
     /// destroys application state
     ///
-    /// - saves state to `~/.local/share/enkei/state.json`
+    /// - saves state to `~/.local/share/enkei/state.json` on linux
+    /// - or `~/Library/Application Support/dev.catgardens.eneki/state.json` on macos
     pub fn destroy(&mut self) -> anyhow::Result<()> {
         self.board.save_state()?;
         Ok(())
